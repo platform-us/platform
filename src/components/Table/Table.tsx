8877fff034763972e0db13e1a649d8f63ca36bdf
Samuel Newman <mozzius@protonmail.com>
2020-07-06 02:31:48 +0100

    added table component

---
+++ src/components/Table/Table.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const StyledTable = styled.table`
+  width: 100%;
+
+  thead > tr > th {
+    text-transform: uppercase;
+    font-size: 12px;
+    color: #707070;
+  }
+`;
+
+export interface TableProps {
+  header: {
+    title: string;
+    component: ({ children }: { children: any }) => JSX.Element;
+  }[];
+  rows: {
+    // additional data for each row can go here
+    cells: {
+      value: any;
+    }[];
+  }[];
+}
+
+const msg = 'Must be an equal number of column in the header and the body';
+
+const Table: React.FC<TableProps> = ({ header, rows }) => {
+  return (
+    <StyledTable>
+      <thead>
+        <tr>
+          {header.map(({ title }) => (
+            <th>{title}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map(row => {
+          if (header.length !== row.cells.length) throw new Error(msg);
+          return (
+            <tr>
+              {row.cells.map((cell, i) => {
+                const Component = header[i].component;
+                return (
+                  <td>
+                    <Component>{cell.value}</Component>
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </StyledTable>
+  );
+};
+
+export default Table;
